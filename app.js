df9ca591cb1b8d2c7991aceb46ae13249d9d9995
Aryan Shafa Wardana <yan@Aryans-MacBook-Air.local>
2022-01-31 18:45:04 +0700

    More accurate timer

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
-// Selectors
+//Selectors
 const activityInput = document.querySelector(".activity-input")
 const activityButton = document.querySelector(".activity-button")
 const activityList = document.querySelector(".activity-list")
 
-// Event Listeners
+//Event Listeners
 document.addEventListener("DOMContentLoaded", getActivities)
 activityButton.addEventListener("click", addActivity)
 
-// Functions
+//Functions
 function addActivity(event) {
     event.preventDefault()
+    
+    //Getting activities for below reasons
     let activities = localActivityInit()
     var activityName = activityInput.value
 
+    //If there is already a duplicate name
     while (activities.find((element) => { return element.name === activityName}) != undefined) {
         activityName = activityName + " copy"
     }
 
+    //If input is empty don't run function
     if (!activityName) return
 
-    // Activity div
+    //Initialise Activity Div
     const activityDiv = document.createElement("div")
     activityDiv.classList.add("activity")
 
-    // Activity name
+    //Initialise Activity Name
     const newActivity = document.createElement("li")
     newActivity.innerText = activityName
     activityDiv.appendChild(newActivity)
 
-    //add activitie
+    //Add Activity
     saveLocalActivities(activityName)
 
     // Stopwatch
     const stopwatchDiv = document.createElement("div")
     stopwatchDiv.innerText = "00:00:00"
     stopwatchDiv.classList.add("timer")
-    stopwatchDiv.stopwatch = new Stopwatch(stopwatchDiv, 0, 0, 0)
+    stopwatchDiv.stopwatch = new Timer(stopwatchDiv, false, 0)
     activityDiv.appendChild(stopwatchDiv)
 
     // Stopwatch toggle button
@@ -72,7 +76,7 @@ function toggleActivitySW(event) {
 
     const timer = item.previousSibling
 
-    timer.stopwatch.toggleClock()
+    timer.stopwatch.toggle()
 }
 
 function resetActivitySW(event) {
@@ -80,82 +84,116 @@ function resetActivitySW(event) {
 
     const timer = item.previousSibling.previousSibling
 
-    timer.stopwatch.resetClock()
+    timer.stopwatch.reset()
 }
 
 function removeActivity(event) {
-    console.log(event.target)
     event.target.parentElement.remove()
     removeLocalActivity(event.target)
 }
 
-function Stopwatch(element, hr, min, sec) {
-    this.timer = element
-    this.hr = hr
-    this.min = min
-    this.sec = sec
-    this.stoptime = true
-    this.interval = null
+//Timer class and logic
+class Timer {
+    constructor (element, isRunning, overallTime) {
+        this.timer = element;
+        this.isRunning = isRunning;
+        this.startTime = 0;
+        this.overallTime = overallTime;
+        this.interval = null;
 
-    this.toggleClock = () => {
-        if (this.stoptime) {
-            this.stoptime = false
+        if (overallTime > 0 || this.isRunning) this.startTime = Date.now()
+
+        this.updateDiv()
+        if (this.isRunning) {
+            this.updateTime()
+        }
+    }
+
+    _getTimeElapsedSinceLastStart() {
+        if (!this.startTime) return 0;
+        
+        return Date.now() - this.startTime;
+    }
+
+    toggle() {
+        console.log("Test")
+        if (!this.isRunning) {
+            this.isRunning = true;
             this.timer.nextSibling.children[0].setAttribute("class", "fas fa-pause")
 
-            this.clockCycle()
+            this.startTime = Date.now();
+            this.updateTime()
         } else {
-            this.stoptime = true
+            this.isRunning = false;
             this.timer.nextSibling.children[0].setAttribute("class", "fa fa-play")
-            clearTimeout(this.timeout)
+
+            this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+            this.saveTime()
+            clearInterval(this.interval)
         }
     }
 
-    this.clockCycle = () => {
-        if (this.stoptime) return
-
-        this.sec = parseInt(this.sec)
-        this.min = parseInt(this.min)
-        this.hr = parseInt(this.hr)
-
-        this.sec += 1
-
-        if (this.sec == 60) {
-            this.min += 1
-            this.sec = 0
-        }
-        if (this.min == 60) {
-            this.hr += 1
-            this.min = 0
+    reset() {
+        this.overallTime = 0;
+        if (this.isRunning) {
+            this.startTime = Date.now();
+            return;
         }
 
-        if (this.sec < 10) {
-            this.sec = '0' + this.sec
-        }
-        if (this.min < 10) {
-            this.min = '0' + this.min
-        }
-        if (this.hr < 10) {
-            this.hr = '0' + this.hr
-        }
-
-        this.timer.innerText = this.hr + ":" + this.min + ":" + this.sec
-
-        var that = this
-        updateLocalActivityTime(this.timer, this.hr, this.min, this.sec)
-        this.timeout = setTimeout(() => { that.clockCycle() }, 1000);
+        this.startTime = 0;
     }
 
-    this.resetClock = () => {
-        if (!this.stoptime) this.toggleClock()
-        this.hr = 0
-        this.min = 0
-        this.sec = 0
-        this.timer.innerHTML = "00:00:00"
-        this.timeout = null
+    getTime() {
+        if (!this.startTime) {
+            return 0;
+        }
+
+        if (this.isRunning) {
+            return this.overallTime + this._getTimeElapsedSinceLastStart();
+        }
+
+        return this.overallTime;
     }
 
+    updateTime() {
+        this.interval = setInterval(() => {
+            this.updateDiv();
+            this.saveTime()
+        }, 100)
+    }
+
+    updateDiv() {
+        const timeInSeconds = Math.round(this.getTime() / 1000);
+
+        let sec = timeInSeconds % 60;
+        let min = Math.floor(timeInSeconds / 60) % 60
+        let hrs = Math.floor(timeInSeconds / 3600)
+
+        if (sec < 10) {
+            sec = '0' + sec
+        }
+        if (min < 10) {
+            min = '0' + min
+        }
+        if (hrs < 10) {
+            hrs = '0' + hrs
+        }
+
+        this.timer.innerText = hrs + ":" + min + ":" + sec
+    }
+
+    saveTime() {
+        updateLocalActivityTime(this.timer, this.isRunning, this.overallTime)
+    }
+
+    getStatus() {
+        if (this.isRunning) {
+            return "fas fa-pause"
+        } else return "fa fa-play"
+    }
 }
 
+//Getting activities from local storage
 function localActivityInit() {
     if (localStorage.getItem("activities") === null) {
         return []
@@ -163,13 +201,37 @@ function localActivityInit() {
         return JSON.parse(localStorage.getItem("activities"))
     }
 }
+
+//Save new activity
 function saveLocalActivities(activity) {
     let activities = localActivityInit()
 
-    activities.push({"name": activity, "hr": 0, "min": 0, "sec": 0})
+    activities.push({"name": activity, "isRunning": false, "overallTime": 0})
     localStorage.setItem("activities", JSON.stringify(activities))
 }
 
+//Remove activity from local storage
+function removeLocalActivity(activity) {
+    let activities = localActivityInit()
+
+    let activityIndex = activities.findIndex((element) => { return element.name === activity.parentElement.children[0].innerText })
+    activities.splice(activityIndex, 1)
+    localStorage.setItem("activities", JSON.stringify(activities))
+}
+
+//Update activities (isRunning, overallTime) in local storage
+function updateLocalActivityTime (timer, isRunning, overallTime) {
+    let activities = localActivityInit()
+
+    let activity = activities.find((element) => { return element.name === timer.parentElement.children[0].innerText })
+
+    activity.isRunning = isRunning
+    activity.overallTime = overallTime
+
+    localStorage.setItem("activities", JSON.stringify(activities))
+}
+
+//Load activities to DOM from local storage
 function getActivities() {
     let activities = localActivityInit()
 
@@ -184,27 +246,15 @@ function getActivities() {
 
         // Stopwatch
         const stopwatchDiv = document.createElement("div")
-        let sec = parseInt(element.sec)
-        let min = parseInt(element.min)
-        let hr = parseInt(element.hr)
-
-        if (sec < 10) {
-            sec = '0' + sec
-        }
-        if (min < 10) {
-            min = '0' + min
-        }
-        if (hr < 10) {
-            hr = '0' + hr
-        }
-        stopwatchDiv.innerText = hr + ":" + min + ":" + sec
+        stopwatchDiv.innerText = "00:00:00"
         stopwatchDiv.classList.add("timer")
-        stopwatchDiv.stopwatch = new Stopwatch(stopwatchDiv, element.hr, element.min, element.sec)
+        stopwatchDiv.stopwatch = new Timer(stopwatchDiv, element.isRunning, element.overallTime)
         activityDiv.appendChild(stopwatchDiv)
 
         // Stopwatch toggle button
+        let icon = stopwatchDiv.stopwatch.getStatus()
         const toggleButton = document.createElement("button")
-        toggleButton.innerHTML = '<i class="fa fa-play"></i>'
+        toggleButton.innerHTML = `<i class="${icon}"></i>`
         toggleButton.classList.add("toggle-btn")
         toggleButton.addEventListener("click", toggleActivitySW)
         activityDiv.appendChild(toggleButton)
@@ -230,24 +280,4 @@ function getActivities() {
         //Resets input
         activityInput.value = ""
     })
-}
-
-function removeLocalActivity(activity) {
-    let activities = localActivityInit()
-
-    let activityIndex = activities.findIndex((element) => { return element.name === activity.parentElement.children[0].innerText })
-    activities.splice(activityIndex, 1)
-    localStorage.setItem("activities", JSON.stringify(activities))
-}
-
-function updateLocalActivityTime (timer, hr, min, sec) {
-    let activities = localActivityInit()
-
-    let activity = activities.find((element) => { return element.name === timer.parentElement.children[0].innerText })
-
-    activity.hr = hr
-    activity.min = min
-    activity.sec = sec
-
-    localStorage.setItem("activities", JSON.stringify(activities))
 }
